971bc354395baf016f5a178fc7b09f8ddcb9787e
Nicolas Rocha Pacheco <n.rocha11@uniandes.edu.co>
2021-12-11 11:21:59 -0500

    Added router and routes to pages

--- website/src/App.js
+++ website/src/App.js
@@ -1,10 +1,25 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from 'react-router-dom';
 
+import Home from "./pages/Home";
+import Skills from "./pages/Skills";
 import NavBar from "./components/Navbar";
+import Education from "./pages/Education";
 
 function App() {
   return (
-    <NavBar />
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/education" element={<Education />} />
+        <Route path="/skills" element={<Skills />} />
+      </Routes>
+    </Router>
   );
 }
 
