675a3ab00a6a969beb0e5f18fae035d0fa04bdd1
Nicolas Rocha Pacheco <n.rocha11@uniandes.edu.co>
2021-12-11 18:21:30 -0500

    Finished home page for personal website

--- website/src/pages/Home.js
+++ website/src/pages/Home.js
@@ -1,10 +1,29 @@
-
-
+import "./Home.css";
+import "./Pages.css";
+import myself from "../myself.png";
 
 function Home(){
     return (
-        <div>
-            This is my home
+        <div className="page">
+            <div className="home-banner">
+                <div className="home-banner-header">
+                    <div className="home-banner-name">Nicolás<br />Rocha Pacheco</div>
+                    <div className="home-banner-title">Hardware Engineer</div>
+                </div>
+                <div className="home-banner-photo">
+                    <img src={myself} alt="myself" />
+                </div>
+            </div>
+            <div className="home-invitation">
+                I welcome you to my personal webpage. In this page I would like to
+                introduce myself in terms of my education, experience and skills. 
+                <br /><br />
+                I consider myself to be a hardware engineer i.e. the person that develops
+                electronics that have the potential to change the life of billions of
+                people. 
+                <br /><br />
+                Feel free to navigate to know more about myself.
+            </div>
         </div>
     );
 }
