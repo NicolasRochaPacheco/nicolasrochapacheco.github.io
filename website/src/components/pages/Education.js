f5d3596ce0b8e2b7a3bc3bbfaa5cc08ca9cbb747
Nicolas Rocha Pacheco <n.rocha11@uniandes.edu.co>
2021-12-18 11:21:55 -0500

    Moved education page files to new folder

--- website/src/components/pages/Education.js
+++ website/src/components/pages/Education.js
@@ -5,44 +5,44 @@ import "./Education.css";
 function Education(){
     return (
         <div className="page education">
-            <h1 className="page-title">Academic Degrees</h1>
+            <h1 className="page-title">MY ACADEMIC DEGREES</h1>
             <div className="education-wrapper">
                 <div className="education-item">
                     <div className="education-item-title">Electrical Engineering</div>
                     <div className="education-item-kind">Bachelor of Science (ABET Accredited)</div>
-                    <hr className="education-hr" />
+                    <hr className="page-hr" />
                     <div className="education-item-university">Universidad de Los Andes</div>
                     <div className="education-item-location">Bogotá, Colombia</div>
-                    <hr className="education-hr" />
+                    <hr className="page-hr" />
                     <div className="education-item-coursework"><b>Relevant Coursework:</b> Renewable Energy, Power Electronics. </div>
                 </div>
 
                 <div className="education-item">
                     <div className="education-item-title">Electronics Engineering</div>
                     <div className="education-item-kind">Bachelor of Science (ABET Accredited)</div>
-                    <hr className="education-hr" />
+                    <hr className="page-hr" />
                     <div className="education-item-university">Universidad de Los Andes</div>
                     <div className="education-item-location">Bogotá, Colombia</div>
-                    <hr className="education-hr" />
+                    <hr className="page-hr" />
                     <div className="education-item-coursework"><b>Relevant Coursework:</b> Electronic Digital Systems Design and Architecture, Electronic Digital Systems. </div>
                 </div>
 
                 <div className="education-item">
                     <div className="education-item-title">Electronics and Computer Engineering</div>
                     <div className="education-item-kind">Master of Science</div>
-                    <hr className="education-hr" />
+                    <hr className="page-hr" />
                     <div className="education-item-university">Universidad de Los Andes</div>
                     <div className="education-item-location">Bogotá, Colombia</div>
-                    <hr className="education-hr" />
+                    <hr className="page-hr" />
                     <div className="education-item-coursework"><b>Relevant Coursework:</b> Machine Learning, Embedded Systems, Optimization, Natural Language Processing. </div>
                 </div>
             </div>
 
-            <h1 className="page-title">Self Learning</h1>
+            <h1 className="page-title">RELEVANT SELF LEARNING KNOWLEDGE</h1>
             <div className="education-wrapper">
                 <div className="education-item">
                     <div className="education-self-title">Computer Architecture</div>
-                    <hr className="education-hr" />
+                    <hr className="page-hr" />
                     <div className="education-self-desc">I wanted to answer myself "How 
                     a computer processor works?". That lead me to research the organization
                     of a processor and how it is optimized depending design requirements 
@@ -53,7 +53,7 @@ function Education(){
                 </div>
                 <div className="education-item">
                     <div className="education-self-title">Verilog</div>
-                    <hr className="education-hr" />
+                    <hr className="page-hr" />
                     <div className="education-self-desc">Learning the ideas behind a processor
                     are one thing but implementing them is another story. I wanted to improve 
                     my Verilog skills acquired during my undergrad studies. My ability with the
@@ -63,7 +63,7 @@ function Education(){
                 </div>
                 <div className="education-item">
                     <div className="education-self-title">Design Verification</div>
-                    <hr className="education-hr" />
+                    <hr className="page-hr" />
                     <div className="education-self-desc">My Master's thesis thaught me that
                     hardware development required more verification steps than design itself.
                     Since my Master's thesis I've been researching and learning how modern
@@ -72,33 +72,33 @@ function Education(){
                     be used to model digital systems on different abstraction levels.</div>
                 </div>
             </div>
-            <h1 className="education-title">Certifications</h1>
+            <h1 className="education-title">OTHER CERTIFICATIONS</h1>
             <div className="education-wrapper">
                 <div className="education-item">
                     <div className="education-item-title">Graphic Design</div>
                     <div className="education-item-kind">Course Certificate</div>
-                    <hr className="education-hr" />
+                    <hr className="page-hr" />
                     <div className="education-item-university">Coursera</div>
                     <div className="education-item-location">Online</div>
-                    <hr className="education-hr" />
+                    <hr className="page-hr" />
                     <div className="education-item-coursework">Learned basic principles of graphic design to improve my visual communication skills.</div>
                 </div>
                 <div className="education-item">
                     <div className="education-item-title">DeepLearning.AI TensorFlow Developer</div>
                     <div className="education-item-kind">Professional Certificate</div>
-                    <hr className="education-hr" />
+                    <hr className="page-hr" />
                     <div className="education-item-university">Coursera</div>
                     <div className="education-item-location">Online</div>
-                    <hr className="education-hr" />
+                    <hr className="page-hr" />
                     <div className="education-item-coursework">Learned how to implement different deep learning models with TensorFlow.</div>
                 </div>
                 <div className="education-item">
                     <div className="education-item-title">Build Your First React Website</div>
                     <div className="education-item-kind">Guided Project</div>
-                    <hr className="education-hr" />
+                    <hr className="page-hr" />
                     <div className="education-item-university">Coursera</div>
                     <div className="education-item-location">Online</div>
-                    <hr className="education-hr" />
+                    <hr className="page-hr" />
                     <div className="education-item-coursework">Learned React.js basic principles and syntax.</div>
                 </div>
             </div>
